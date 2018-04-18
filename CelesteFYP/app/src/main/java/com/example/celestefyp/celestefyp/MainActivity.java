2e2845b57cba14a5fd2466b32723a9ddff37a1cf
Edward <t102590050@ntut.org.tw>
2018-04-18 16:18:51 +0800

    Adding Bluetooth
    - Bluetooth related code

--- CelesteFYP/app/src/main/java/com/example/celestefyp/celestefyp/MainActivity.java
+++ CelesteFYP/app/src/main/java/com/example/celestefyp/celestefyp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.celestefyp.celestefyp;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -25,8 +28,17 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
     int RESULT_LOAD_IMAGE = 0;
     private static final int MY_PERMISSIONS_REQUEST_STORAGE = 1;
     int soundValue0=0,soundValue1=0,soundValue2=0;
@@ -120,6 +132,66 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter= BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Device doesn't Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(iterator.getName()))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return connected;
+    }
+
     @Override
     public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -193,10 +265,20 @@ public class MainActivity extends AppCompatActivity {
             soundValue2 = tempColor;
         }
     }
+    public void outPutToArduino(String colorName){
+        colorName.concat("\n");
+        try {
+            outputStream.write(colorName.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        BTinit();
+        BTconnect();
         //super.onActivityResult(requestCode, resultCode, data);
         if(this.requestCode == requestCode && resultCode == RESULT_OK){
             Bitmap bitmap = (Bitmap)data.getExtras().get("data");
