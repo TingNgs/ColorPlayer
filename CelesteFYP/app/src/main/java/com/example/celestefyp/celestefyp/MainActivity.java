9e92df115bda0f10f0117019d2b5bdee5c6d7c84
Edward <t102590050@ntut.org.tw>
2018-05-25 23:49:12 +0800

    Modify Mac Address

--- CelesteFYP/app/src/main/java/com/example/celestefyp/celestefyp/MainActivity.java
+++ CelesteFYP/app/src/main/java/com/example/celestefyp/celestefyp/MainActivity.java
@@ -49,7 +49,7 @@ import java.util.jar.Attributes;
 public class MainActivity extends AppCompatActivity{
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-    private final String btDeviceName = "HC-05";
+    private final String DEVICE_ADDRESS="98:D3:31:FD:62:33";
     private BluetoothDevice device;
     private BluetoothSocket socket;
     private OutputStream outputStream;
@@ -290,7 +290,6 @@ public class MainActivity extends AppCompatActivity{
                 deviceConnected = true;
             }
         }
-
     }
     View.OnClickListener colorPicker0_OnClick = new View.OnClickListener() {
         @Override
@@ -356,7 +355,7 @@ public class MainActivity extends AppCompatActivity{
 
             for (BluetoothDevice iterator : bondedDevices)
             {
-                if(btDeviceName.equals(iterator.getName()))
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
                 {
                     device=iterator;
                     found=true;
